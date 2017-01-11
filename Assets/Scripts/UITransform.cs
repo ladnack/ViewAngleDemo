342b5ef297ebb95846c6642daa2a13abaece7ddd
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-01-11 23:50:10 +0900

    Add script to appear UI canvas

---
+++ Assets/Scripts/UITransform.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UITransform : MonoBehaviour {
+
+	public Camera _camera;
+
+	public float distance = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		if (_camera == null) {
+			_camera = GameObject.FindGameObjectWithTag("Main Camera").GetComponent<Camera>();
+		}
+
+		CreateCanvasAroundCamaraForAngle (_camera, -5f, -5f, 0f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// カメラの正面方向から指定した角度の方向にオブジェクトを出現させる
+	private void CreateCanvasAroundCamaraForAngle(Camera cam, float x, float y, float z) {
+		// forwardに対して回転させたベクトルを求める
+		var angleForward = Quaternion.Euler (x, y, z) * cam.transform.forward;
+		Vector3 objPosition = cam.transform.position + angleForward * distance;
+
+		ApearCanvas (objPosition, cam.transform.forward);
+	}
+
+	// Canvasを表示する
+	private void ApearCanvas(Vector3 pos, Vector3 forward) {
+		var _canvas = GameObject.Find("Canvas");
+		_canvas.transform.position = pos;
+
+		// posのSetは以下も同じ
+		// var rectTransform = _canvas.GetComponent<RectTransform> ();
+		// rectTransform.position = pos;
+
+		// 任意の方向に対して、そのベクトルに垂直になるように角度を調整する
+		_canvas.transform.forward = forward;
+
+	}
+}
