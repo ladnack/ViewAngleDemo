d9d00f92335069d0dc92b13607025cddf8f5d3f4
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-01-11 23:50:54 +0900

    Add script to action for button clicked

---
+++ Assets/Scripts/ButtonFunction.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonFunction : MonoBehaviour {
+
+	public void NoArgFunction()
+	{
+		Debug.Log ("NoArgFunction");
+	}
+
+	public void IntArgFunction(int i)
+	{
+		Debug.Log ("IntArgFunction Called. arg=" + i.ToString ());
+	}
+
+	public void FloatArgFunction(float f)
+	{
+		Debug.Log ("FloatArgFunction Called. arg="+f.ToString());
+	}
+
+	public void StringArgFunction(string s)
+	{
+		Debug.Log ("StringArgFunction Called. arg="+s);
+	}
+
+	public void BoolArgFunction(bool b)
+	{
+		Debug.Log ("BoolArgFunction Called. arg="+b.ToString());
+	}
+
+	public void ObjectArgFunction(Object o)
+	{
+		Debug.Log ("ObjectArgFunction Called. arg="+o.ToString());
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
