5b8120e0dc4f50fbc02f22b96e8eba44e366a955
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-01-11 18:50:21 +0900

    Add script to action for UI interaction

---
+++ Assets/ButtonClickEvent.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ButtonClickEvent : MonoBehaviour {
+
+	[SerializeField] Slider slider;
+	[SerializeField] Button button;
+
+	// Use this for initialization
+	void Start () {
+		slider.onValueChanged.AddListener (OnSlide);
+
+		// button.onClick.AddListener (ShowLog);
+
+		UnityAction onClickAction = ShowLog;
+		button.onClick.AddListener (onClickAction);
+
+		UnityAction onClickAction2 = () => Debug.Log ("Hello UnityAction 2");
+		button.onClick.AddListener (onClickAction2);
+
+		button.onClick.AddListener (() => Debug.Log ("Hello UnityAction 3"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnSlide (float value)
+	{
+		Debug.Log (value);
+
+		var screen = GameObject.Find ("Screen");
+		var screenPosition = screen.transform.position;
+		Vector3 pos = new Vector3 (screenPosition.x, screenPosition.y, value);
+		screen.transform.position = pos;
+	}
+
+	void ShowLog ()
+	{
+		Debug.Log ("Hello UnityAction");
+	}
+
+}
