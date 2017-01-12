1e389c11fadf56a2597e516c4451b7d39839e4c4
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-01-12 23:21:42 +0900

    Add script to get angle of from object to camera

---
+++ Assets/Scripts/GetAngle.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetAngle : MonoBehaviour {
+
+	public GameObject _gameObject;
+	public Camera _camera;
+
+	// Use this for initialization
+	void Start () {
+		if (_gameObject == null) {
+			_gameObject = new GameObject ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Debug.Log ("Space key was pressed!");
+
+			// 視線の向き（頭部の向き）を求める
+			Vector3 lineOfSight = _camera.transform.forward;
+			Debug.Log("lineOfSight: " + lineOfSight);
+
+			// Cameraからコントローラーまでのベクトルを取得する
+			Vector3 objectFromCamera = _gameObject.transform.position - _camera.transform.position;
+			Debug.Log("gameObject.position: " + _gameObject.transform.position);
+			Debug.Log("objectFromCamera.position: " + _camera.transform.position);
+
+			// 視線方向に対するコントローラー角度を求める
+			var angle = Vector3.Angle(lineOfSight, objectFromCamera);
+			Debug.Log("angle: " + angle);
+		}
+
+
+	}
+}
