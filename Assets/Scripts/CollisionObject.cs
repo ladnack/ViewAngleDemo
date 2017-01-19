18a108592cf6e75f0ef7801bf2e6a61aeb9c5a9b
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-01-19 22:23:58 +0900

    Add script to judge collision

---
+++ Assets/Scripts/CollisionObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	private void OnCollisionEnter(Collision collision) {
+		Debug.Log ("OnCollisionEnter!");
+	}
+
+	private void OnCollisionStay(Collision collision) {
+		// Debug.Log ("OnCollisionStay!");
+
+		foreach (ContactPoint contact in collision.contacts) {
+			print(contact.thisCollider.name + " hit " + contact.otherCollider.name);
+			Debug.DrawRay(contact.point, contact.normal, Color.white);
+
+			print("Points colliding: " + collision.contacts.Length);
+			print("First point that collided: " + collision.contacts[0].point);
+
+		}
+	}
+
+}
