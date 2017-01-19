aae694e159fbdf25b249393cc38ff568d5df8aa2
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-01-20 00:16:50 +0900

    Add script to judge collide with UI

--- Assets/Scripts/CollisionObject.cs
+++ Assets/Scripts/CollisionObject.cs
@@ -13,22 +13,47 @@ public class CollisionObject : MonoBehaviour {
 	
 	}
 
-
+	// 衝突判定
 	private void OnCollisionEnter(Collision collision) {
-		Debug.Log ("OnCollisionEnter!");
+		// 衝突したオブジェクトの色をランダムに変える
+		var obj = collision.gameObject;
+		obj.GetComponent<Renderer> ().material.color = new Color (Random.value, Random.value, Random.value);
+
 	}
 
 	private void OnCollisionStay(Collision collision) {
-		// Debug.Log ("OnCollisionStay!");
 
 		foreach (ContactPoint contact in collision.contacts) {
-			print(contact.thisCollider.name + " hit " + contact.otherCollider.name);
-			Debug.DrawRay(contact.point, contact.normal, Color.white);
+			Debug.Log (contact.thisCollider.name + " hit " + contact.otherCollider.name);
+			// Debug.DrawRay(contact.point, contact.normal, Color.white);
 
-			print("Points colliding: " + collision.contacts.Length);
-			print("First point that collided: " + collision.contacts[0].point);
-
+			// Debug.Log ("Points colliding: " + collision.contacts.Length);
+			// Debug.Log ("First point that collided: " + collision.contacts [0].point);
 		}
+	}
+
+	private void OnCollisionExit(Collision collision) {
+		
+	}
+
+
+	// トリガー判定
+	private void OnTriggerEnter(Collider collider) {
+
+		var obj = collider.gameObject;
+		obj.GetComponent<Renderer> ().material.color = new Color (Random.value, Random.value, Random.value);
+
+	}
+
+	private void OnTriggerStay(Collider collider) {
+
+	}
+
+	private void OnTriggerExit(Collider collider) {
+		
+		var obj = collider.gameObject;
+		obj.GetComponent<Renderer> ().material.color = new Color (Random.value, Random.value, Random.value);
+
 	}
 
 }
---
+++ Assets/Scripts/UICollision.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UICollision : MonoBehaviour {
+
+	public GameObject obj;
+
+	private float rate = 0.1f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// オブジェクトをZ方向に前進させる
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			obj.transform.position += new Vector3 (0f, 0f, rate);
+		}
+
+	}
+
+	// トリガー判定
+	private void OnTriggerEnter(Collider collider) {
+		Debug.Log ("On Trigger Enter!");
+		var UIobj = collider.gameObject;
+		UIobj.GetComponent<CanvasRenderer> ().SetColor(new Color (Random.value, Random.value, Random.value));
+			
+	}
+
+	private void OnTriggerStay(Collider collider) {
+
+	}
+
+	private void OnTriggerExit(Collider collider) {
+
+		var UIobj = collider.gameObject;
+		UIobj.GetComponent<CanvasRenderer> ().SetColor(new Color (Random.value, Random.value, Random.value));
+
+	}
+
+
+}
