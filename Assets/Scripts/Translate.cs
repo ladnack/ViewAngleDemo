72fddb619d7f7111a3d39c1d18a26a8ca24a1b25
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-01-20 01:01:48 +0900

    Add script to judge collide with UI

---
+++ Assets/Scripts/Translate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Translate : MonoBehaviour {
+
+	public GameObject obj;
+	private float rate = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// オブジェクトをZ方向に前進させる
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			obj.transform.position += new Vector3 (0f, 0f, rate);
+		}
+
+		// オブジェクトをZ方向に後退させる
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			obj.transform.position -= new Vector3 (0f, 0f, rate);
+		}
+	}
+}
--- Assets/Scripts/UICollision.cs
+++ Assets/Scripts/UICollision.cs
@@ -1,13 +1,9 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class UICollision : MonoBehaviour {
-
-	public GameObject obj;
-
-	private float rate = 0.1f;
-
-
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -15,20 +11,27 @@ public class UICollision : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
+	}
 
-		// オブジェクトをZ方向に前進させる
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			obj.transform.position += new Vector3 (0f, 0f, rate);
-		}
 
+	// 衝突判定
+	private void OnCollisionEnter(Collision collision) {
+		Debug.Log ("On Collision Enter!");
+		// 衝突対象がCanvasの場合、色をランダムに変える
+		var UIobj = collision.gameObject;
+		UIobj.GetComponent<CanvasRenderer> ().SetColor(new Color (Random.value, Random.value, Random.value));
+		Debug.Log (collision.contacts [0].point);
 	}
 
 	// トリガー判定
 	private void OnTriggerEnter(Collider collider) {
 		Debug.Log ("On Trigger Enter!");
+		// 衝突対象がCanvasの場合、色をランダムに変える
 		var UIobj = collider.gameObject;
 		UIobj.GetComponent<CanvasRenderer> ().SetColor(new Color (Random.value, Random.value, Random.value));
-			
+
+
 	}
 
 	private void OnTriggerStay(Collider collider) {
