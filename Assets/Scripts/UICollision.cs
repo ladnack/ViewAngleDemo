89632fb42ad4aff231ede6c03402b85fbf067994
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-01-23 21:06:33 +0900

    Add script to divide handling of canvas and the others in changing color

--- Assets/Scripts/UICollision.cs
+++ Assets/Scripts/UICollision.cs
@@ -18,19 +18,24 @@ public class UICollision : MonoBehaviour {
 	// 衝突判定
 	private void OnCollisionEnter(Collision collision) {
 		Debug.Log ("On Collision Enter!");
-		// 衝突対象がCanvasの場合、色をランダムに変える
+
+		// 衝突したGameObjectを取得
 		var UIobj = collision.gameObject;
-		UIobj.GetComponent<CanvasRenderer> ().SetColor(new Color (Random.value, Random.value, Random.value));
+		// 衝突対象の色をランダムに変更する
+		ChangeObjectColorRandom(UIobj);
+
+
 		Debug.Log (collision.contacts [0].point);
 	}
 
 	// トリガー判定
 	private void OnTriggerEnter(Collider collider) {
 		Debug.Log ("On Trigger Enter!");
-		// 衝突対象がCanvasの場合、色をランダムに変える
-		var UIobj = collider.gameObject;
-		UIobj.GetComponent<CanvasRenderer> ().SetColor(new Color (Random.value, Random.value, Random.value));
 
+		// 接触したGameObjectを取得
+		var UIobj = collider.gameObject;
+		// トリガー対象の色をランダムに変更する
+		ChangeObjectColorRandom(UIobj);
 
 	}
 
@@ -40,9 +45,25 @@ public class UICollision : MonoBehaviour {
 
 	private void OnTriggerExit(Collider collider) {
 
+		// 接触したGameObjectを取得
 		var UIobj = collider.gameObject;
-		UIobj.GetComponent<CanvasRenderer> ().SetColor(new Color (Random.value, Random.value, Random.value));
+		// トリガー対象の色をランダムに変更する
+		ChangeObjectColorRandom(UIobj);
 
+	}
+
+	// GameObjectの色をランダムに変更する
+	private void ChangeObjectColorRandom(GameObject obj) {
+		// objがCanvasRendererを持っていれば、objはCanvasとする
+		var canvasRenderer = obj.GetComponent<CanvasRenderer>();
+
+		if (canvasRenderer != null) {
+			// 衝突対象がCanvasの場合
+			obj.GetComponent<CanvasRenderer> ().SetColor (new Color (Random.value, Random.value, Random.value));
+		} else {
+			// 衝突対象がCanvas以外のGameObjectの場合
+			obj.GetComponent<Renderer> ().material.color = new Color (Random.value, Random.value, Random.value);
+		}
 	}
 
 
--- Assets/Scripts/UITransform.cs
+++ Assets/Scripts/UITransform.cs
@@ -12,6 +12,7 @@ public class UITransform : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		
 		if (_camera == null) {
 			_camera = GameObject.FindGameObjectWithTag("Main Camera").GetComponent<Camera>();
 		}
