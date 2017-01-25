ec3897aeae8cdc9d6acc3fd91069f0c56050b87c
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-01-25 15:36:18 +0900

    Add script to fade object color

---
+++ Assets/Scripts/ColorFade.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ColorFade : MonoBehaviour {
+
+	// private Renderer _renderer;
+	private Button _button;
+	private float alpha;
+
+	// Use this for initialization
+	void Start () {
+		// _renderer = GetComponent<Renderer> ();
+		_button = GetComponent<Button> ();
+
+		/*
+		if (_renderer == null) {
+			Debug.Log ("Not find Renderer.");
+			return;
+		}
+		*/
+
+		if (_button == null) {
+			Debug.Log ("Not find Button.");
+			return;
+		}
+
+		// alpha = _renderer.material.color.a;
+		alpha = _button.colors.normalColor.a;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("f")) {
+			Debug.Log ("Push Key F.");
+			/*
+			if (_renderer != null) {
+				StartCoroutine ("Fade");
+			}
+			*/
+
+			if (_button != null) {
+				StartCoroutine ("Fade");
+			}
+		}
+	}
+
+
+	IEnumerator Fade() {
+		for (float f = 1f; f >= 0; f -= 0.1f) {
+			/*
+			Color _color = _renderer.material.color;
+			_color.a = f;
+			_renderer.material.color = _color;
+			*/
+
+			Color _color = _button.colors.normalColor;
+			_color.a = f;
+			alpha = _color.a;
+			ColorBlock colors = _button.colors;
+			colors.normalColor = _color;
+			_button.colors = colors;
+			Debug.Log ("Update alpha = " + _color.a);
+			yield return null;
+		}
+	}
+
+	void OnGUI() {
+		GUI.Box (new Rect (0, 0, 100, 50), "alpha = " + alpha);
+	}
+
+
+}
