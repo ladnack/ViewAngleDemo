1719fd65dbac7a24315547c52be225ea3806b229
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-01-26 15:08:53 +0900

    Add script to cause button OnClick event

---
+++ Assets/Scripts/ButtonCustomOnClick.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonCustomOnClick : MonoBehaviour {
+
+
+	public void LabelText (string str) {
+		Text text = GameObject.Find ("Text").GetComponent<Text> ();
+		text.text = str;
+		Debug.Log ("LabelText()");
+	}
+
+}
