86b2bd26e072a9165ac0ec74574ea43c7600b739
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-01-26 16:22:15 +0900

    Add script to csuse button evnet by clicking another button

--- Assets/Scripts/ButtonCustomOnClick.cs
+++ Assets/Scripts/ButtonCustomOnClick.cs
@@ -5,10 +5,17 @@ using UnityEngine.UI;
 public class ButtonCustomOnClick : MonoBehaviour {
 
 
-	public void LabelText (string str) {
-		Text text = GameObject.Find ("Text").GetComponent<Text> ();
+	public void SetLabelText (string str) {
+		Text text = GameObject.Find ("MainText").GetComponent<Text> ();
 		text.text = str;
 		Debug.Log ("LabelText()");
 	}
+
+	public void IndirectOnClick(){
+		Button mainButton = GameObject.Find ("MainButton").GetComponent<Button> ();
+		// 別のbuttonのイベント処理を引き起こす
+		mainButton.onClick.Invoke ();
+	}
+
 
 }
