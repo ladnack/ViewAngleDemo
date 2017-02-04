b2c45476e9a2b8b9e9fab7310fa7811c33dece30
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-02-04 19:03:25 +0900

    Add script to get mesh data of game object

---
+++ Assets/Scripts/AnalyzeMesh.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnalyzeMesh : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Mesh mesh = gameObject.GetComponent<MeshFilter>().mesh;
+
+		for(int i = 0; i < mesh.vertices.Length; i++)
+		{
+			print("vertices[" + i + "] : " + mesh.vertices[i]);
+		}
+
+		for(int i = 0; i < mesh.uv.Length; i++)
+		{
+			print("uv[" + i + "] : " + mesh.uv[i]);
+		}
+
+		for(int i = 0; i < mesh.triangles.Length; i++)
+		{
+			print("triangles[" + i + "] : " + mesh.triangles[i]);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
