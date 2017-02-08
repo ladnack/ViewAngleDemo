712e2846aaea75564c66bf0b7f581d6f545e5951
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-02-08 17:32:34 +0900

    Add script to rotate camera by pushing key

--- Assets/Scripts/Raycasting.cs
+++ Assets/Scripts/Raycasting.cs
@@ -5,6 +5,10 @@ public class Raycasting : MonoBehaviour {
 
 	Camera mainCamera;
 
+	[SerializeField] private float rotateX = 1f;
+	[SerializeField] private float rotateY = 1f;
+
+
 	// Use this for initialization
 	void Start () {
 		mainCamera = Camera.main;
@@ -12,6 +16,28 @@ public class Raycasting : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		CameraRotate ();
+
+
 	}
+
+
+	private void CameraRotate() {
+		
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			mainCamera.transform.Rotate(Vector3.up, rotateY);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			mainCamera.transform.Rotate(Vector3.up, -rotateY);
+		}
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			mainCamera.transform.Rotate(Vector3.right, -rotateX);
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			mainCamera.transform.Rotate(Vector3.right, rotateX);
+		}
+
+	}
+
 }
