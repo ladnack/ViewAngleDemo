0013c7df83449e873017688844f063310366dd59
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-02-08 16:05:02 +0900

    Add new scene ‘RaycastCamera’

---
+++ Assets/Scripts/Raycasting.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Raycasting : MonoBehaviour {
+
+	Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
